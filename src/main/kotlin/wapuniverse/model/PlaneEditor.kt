ed1935d84ceb233c9a540042b3cd5d3107ac5fb0
cubuspl42 <cubuspl42@gmail.com>
2018-09-07 18:38:53 +0200

    Add camera

--- src/main/kotlin/wapuniverse/model/PlaneEditor.kt
+++ src/main/kotlin/wapuniverse/model/PlaneEditor.kt
@@ -1,7 +1,10 @@
 package wapuniverse.model
 
+import wapuniverse.geom.Vec2i
+import wapuniverse.util.objectProperty
+
 class PlaneEditor(
         val plane: Plane
 ) {
-
+    val cameraOffset = objectProperty(Vec2i())
 }
