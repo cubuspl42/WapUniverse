7c32bfcce0fc1b355ed4c24bba434c043874c5b4
cubuspl42 <cubuspl42@gmail.com>
2018-09-08 00:54:45 +0200

    Refactor world view

--- src/main/kotlin/wapuniverse/view/WorldView.kt
+++ src/main/kotlin/wapuniverse/view/WorldView.kt
@@ -1,53 +1,16 @@
 package wapuniverse.view
 
 import javafx.beans.value.ObservableObjectValue
-import javafx.scene.Node
 import javafx.scene.image.Image
-import javafx.scene.image.ImageView
-import javafx.scene.input.MouseButton
-import javafx.scene.input.MouseEvent
-import javafx.scene.layout.Pane
-import org.reactfx.EventStreams.eventsOf
-import org.reactfx.Subscription
-import wapuniverse.geom.Vec2d
 import wapuniverse.model.PlaneEditor
 import wapuniverse.rez.RezImageProvider
-import wapuniverse.view.extensions.map
-import wapuniverse.view.extensions.toObservableList
-import wapuniverse.view.util.group
+import wapuniverse.view.util.loadFxml
 import wapuniverse.view.util.observableValue
 
-fun worldView(planeEditor: PlaneEditor, rezImageProvider: RezImageProvider): Node {
-    val plane = planeEditor.plane
-    val tileNodes = plane.tiles.toObservableList { index, tileId ->
-        ImageView().apply {
-            x = index.x * 64.0
-            y = index.y * 64.0
-            imageProperty().bind(provideTileImage(rezImageProvider, plane.world.imageDir, plane.imageSet, tileId))
-        }
-    }
-    return Pane(group(tileNodes).apply {
-        translateXProperty().bind(planeEditor.cameraOffset.map { -it.x })
-        translateYProperty().bind(planeEditor.cameraOffset.map { -it.y })
-    }).apply {
-        setOnMousePressed { e1 ->
-            if (e1.button == MouseButton.SECONDARY) {
-                val cameraStartPosition = planeEditor.cameraOffset.value
-                val startPosition = Vec2d(e1.x, e1.y)
-                val s1 = eventsOf(this, MouseEvent.MOUSE_DRAGGED).subscribe { e2 ->
-                    val newPosition = Vec2d(e2.x, e2.y)
-                    val delta = newPosition - startPosition
-                    planeEditor.cameraOffset.value = cameraStartPosition - delta.toVec2i()
-                }
-                var s2: Subscription? = null
-                s2 = eventsOf(this, MouseEvent.MOUSE_RELEASED).subscribe {
-                    s1.unsubscribe()
-                    s2!!.unsubscribe()
-                }
-            }
-        }
-    }
-}
+private const val FXML = "/view/WorldView.fxml"
+
+fun worldView(planeEditor: PlaneEditor, rezImageProvider: RezImageProvider) =
+        loadFxml(FXML) { WorldViewController(planeEditor, rezImageProvider) }
 
 fun provideTileImage(
         rezImageProvider: RezImageProvider,
