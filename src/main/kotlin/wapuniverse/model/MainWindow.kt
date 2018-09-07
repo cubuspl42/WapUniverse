9eae3f2760c0a5ad9ca86616c27f48f9dd9c72be
cubuspl42 <cubuspl42@gmail.com>
2018-09-07 13:55:27 +0200

    Add coroutines support

--- src/main/kotlin/wapuniverse/model/MainWindow.kt
+++ src/main/kotlin/wapuniverse/model/MainWindow.kt
@@ -1,22 +1,29 @@
 package wapuniverse.model
 
 import javafx.beans.value.ObservableValue
+import kotlinx.coroutines.experimental.Job
+import kotlinx.coroutines.experimental.launch
 import wapuniverse.model.util.dialogProperty
 import wapuniverse.util.optionalProperty
 import java.nio.file.Path
+import kotlinx.coroutines.experimental.javafx.JavaFx as UI
 
 class MainWindow {
-    val newAction = Action { createNewWorld() }
+    val newAction: Action
 
-    val openAction = Action { openWorld() }
+    val openAction: Action
 
-    val saveAction = Action {}
+    val saveAction: Action
 
     val newWorldDialog: ObservableValue<NewWorldDialog?>
 
     val openWorldDialog: ObservableValue<OpenWorldDialog?>
 
     val editor = optionalProperty<Editor?>()
+
+    private val lock = Lock()
+
+    private val job = Job()
 
     private val mNewWorldDialog = dialogProperty<NewWorldDialog>()
 
@@ -25,23 +32,38 @@ class MainWindow {
     private val editorFactory = EditorFactory()
 
     init {
+        newAction = action { createNewWorld() }
+        openAction = action { openWorld() }
+        saveAction = action {}
         newWorldDialog = mNewWorldDialog
         openWorldDialog = mOpenWorldDialog
     }
 
-    private fun createNewWorld() {
-        check(newWorldDialog.value == null)
+    private fun action(block: suspend () -> Unit) = Action(lock.isUnlocked) {
+        launch {
+            lock.lockNow()
+            block()
+            lock.unlock()
+        }
+    }
+
+    private fun launch(block: suspend () -> Unit): Job {
+        return launch(UI, parent = job) {
+            block()
+        }
+    }
+
+    private suspend fun createNewWorld() {
         saveCurrentWorld()
         mNewWorldDialog.set(NewWorldDialog(this))
     }
 
-    private fun openWorld() {
-        check(openWorldDialog.value == null)
+    private suspend fun openWorld() {
         saveCurrentWorld()
         mOpenWorldDialog.set(OpenWorldDialog(this))
     }
 
-    private fun saveCurrentWorld() {
+    private suspend fun saveCurrentWorld() {
         editor.value?.let {
             if (!it.isSaved()) {
                 it.save()
