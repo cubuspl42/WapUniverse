056cbdca136957b169b64067d3791679d313a8ae
cubuspl42 <cubuspl42@gmail.com>
2018-10-26 14:19:48 +0200

    Bind text fields in Edit Object dialog

--- src/main/kotlin/wapuniverse/model/EditObjectDialog.kt
+++ src/main/kotlin/wapuniverse/model/EditObjectDialog.kt
@@ -1,15 +1,64 @@
 package wapuniverse.model
 
 import javafx.beans.property.SimpleIntegerProperty
+import javafx.beans.property.SimpleStringProperty
 import org.fxmisc.easybind.EasyBind.combine
 import wapuniverse.geom.Vec2i
 
 class EditObjectDialog(
         private val editObjectContext: EditObjectContext
 ) : Dialog() {
-    val x = SimpleIntegerProperty(editObjectContext.wapObject.position.value.x)
+    private val wapObject = editObjectContext.wapObject
 
-    val y = SimpleIntegerProperty(editObjectContext.wapObject.position.value.y)
+    private val data = wapObject.data.value
+
+    val id = SimpleIntegerProperty(data.id)
+
+    val name = SimpleStringProperty(data.name)
+
+    val logic = SimpleStringProperty(data.logic)
+
+    val imageSet = SimpleStringProperty(data.imageSet)
+
+    val animation = SimpleStringProperty(data.animation)
+
+    val x = SimpleIntegerProperty(data.x)
+
+    val y = SimpleIntegerProperty(data.y)
+
+    val z = SimpleIntegerProperty(data.z)
+
+    val i = SimpleIntegerProperty(data.i)
+
+    val score = SimpleIntegerProperty(data.score)
+
+    val points = SimpleIntegerProperty(data.points)
+
+    val smarts = SimpleIntegerProperty(data.smarts)
+
+    val powerup = SimpleIntegerProperty(data.powerup)
+
+    val damage = SimpleIntegerProperty(data.damage)
+
+    val health = SimpleIntegerProperty(data.health)
+
+    val speedX = SimpleIntegerProperty(data.speedX)
+
+    val speedY = SimpleIntegerProperty(data.speedY)
+
+    val faceDir = SimpleIntegerProperty(data.faceDir)
+
+    val xMin = SimpleIntegerProperty(data.xMin)
+
+    val xMax = SimpleIntegerProperty(data.xMax)
+
+    val direction = SimpleIntegerProperty(data.direction)
+
+    val yMin = SimpleIntegerProperty(data.yMin)
+
+    val yMax = SimpleIntegerProperty(data.yMax)
+
+    val speed = SimpleIntegerProperty(data.speed)
 
     init {
         combine(x, y) { x, y ->
--- src/main/kotlin/wapuniverse/view/EditObjectDialogController.kt
+++ src/main/kotlin/wapuniverse/view/EditObjectDialogController.kt
@@ -1,5 +1,7 @@
 package wapuniverse.view
 
+import javafx.beans.property.IntegerProperty
+import javafx.beans.property.StringProperty
 import javafx.fxml.FXML
 import javafx.fxml.Initializable
 import javafx.scene.control.Button
@@ -13,19 +15,115 @@ class EditObjectDialogController(
         private val editObjectDialog: EditObjectDialog
 ) : Initializable {
     @FXML
+    lateinit var idTextField: TextField
+
+    @FXML
+    lateinit var nameTextField: TextField
+
+    @FXML
+    lateinit var logicTextField: TextField
+
+    @FXML
+    lateinit var imageSetTextField: TextField
+
+    @FXML
+    lateinit var animationTextField: TextField
+
+    @FXML
     lateinit var xTextField: TextField
 
     @FXML
     lateinit var yTextField: TextField
 
     @FXML
+    lateinit var zTextField: TextField
+
+    @FXML
+    lateinit var iTextField: TextField
+
+    @FXML
+    lateinit var scoreTextField: TextField
+
+    @FXML
+    lateinit var pointsTextField: TextField
+
+    @FXML
+    lateinit var smartsTextField: TextField
+
+    @FXML
+    lateinit var powerupTextField: TextField
+
+    @FXML
+    lateinit var damageTextField: TextField
+
+    @FXML
+    lateinit var healthTextField: TextField
+
+    @FXML
+    lateinit var speedXTextField: TextField
+
+    @FXML
+    lateinit var speedYTextField: TextField
+
+    @FXML
+    lateinit var faceDirTextField: TextField
+
+    @FXML
+    lateinit var xMinTextField: TextField
+
+    @FXML
+    lateinit var xMaxTextField: TextField
+
+    @FXML
+    lateinit var directionTextField: TextField
+
+    @FXML
+    lateinit var yMinTextField: TextField
+
+    @FXML
+    lateinit var yMaxTextField: TextField
+
+    @FXML
+    lateinit var speedTextField: TextField
+
+    @FXML
     lateinit var saveButton: Button
 
     override fun initialize(location: URL?, resources: ResourceBundle?) {
-        xTextField.textProperty().value = editObjectDialog.x.value.toString()
-        editObjectDialog.x.bind(xTextField.textProperty().map { it.toInt() })
-        yTextField.textProperty().value = editObjectDialog.y.value.toString()
-        editObjectDialog.y.bind(yTextField.textProperty().map { it.toInt() })
+        bind(idTextField, editObjectDialog.id)
+        bind(nameTextField, editObjectDialog.name)
+        bind(logicTextField, editObjectDialog.logic)
+        bind(imageSetTextField, editObjectDialog.imageSet)
+        bind(animationTextField, editObjectDialog.animation)
+        bind(xTextField, editObjectDialog.x)
+        bind(yTextField, editObjectDialog.y)
+        bind(zTextField, editObjectDialog.z)
+        bind(iTextField, editObjectDialog.i)
+        bind(scoreTextField, editObjectDialog.score)
+        bind(pointsTextField, editObjectDialog.points)
+        bind(smartsTextField, editObjectDialog.smarts)
+        bind(powerupTextField, editObjectDialog.powerup)
+        bind(damageTextField, editObjectDialog.damage)
+        bind(healthTextField, editObjectDialog.health)
+        bind(speedXTextField, editObjectDialog.speedX)
+        bind(speedYTextField, editObjectDialog.speedY)
+        bind(faceDirTextField, editObjectDialog.faceDir)
+        bind(xMinTextField, editObjectDialog.xMin)
+        bind(xMaxTextField, editObjectDialog.xMax)
+        bind(directionTextField, editObjectDialog.direction)
+        bind(yMinTextField, editObjectDialog.yMin)
+        bind(yMaxTextField, editObjectDialog.yMax)
+        bind(speedTextField, editObjectDialog.speed)
         saveButton.setOnAction { editObjectDialog.save() }
     }
+}
+
+private fun bind(textField: TextField, intValue: IntegerProperty) {
+    textField.textProperty().value = intValue.value.toString()
+    intValue.bind(textField.textProperty().map { it.toInt() })
+}
+
+private fun bind(textField: TextField, stringValue: StringProperty) {
+    textField.textProperty().value = stringValue.value.toString()
+    stringValue.bind(textField.textProperty())
 }
