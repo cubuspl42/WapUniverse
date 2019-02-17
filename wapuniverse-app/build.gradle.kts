99929903740fee7cf372427d01d9c6f1846b1574
cubuspl42 <cubuspl42@gmail.com>
2019-02-17 14:36:38 +0100

    Add EXE task to Gradle

--- wapuniverse-app/build.gradle.kts
+++ wapuniverse-app/build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     application
     kotlin("jvm")
+    id("edu.sc.seis.launch4j") version "2.4.4"
 }
 
 application {
@@ -16,4 +17,21 @@ dependencies {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.register<Jar>("uberJar") {
+    from(sourceSets.main.get().output)
+
+    dependsOn(configurations.runtimeClasspath)
+
+    from(
+        configurations.runtimeClasspath.get()
+                .filter { it.name.endsWith("jar") }
+                .map { zipTree(it) }
+    )
+}
+
+launch4j {
+    mainClassName = "wapuniverse.app.MyApplication"
+//    icon = "${projectDir}/icons/myApp.ico"
 }
