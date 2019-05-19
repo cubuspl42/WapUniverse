c046fc6fa16174986b2774ee7a8cf8ec81c40841
Jakub Trzebiatowski <cubuspl42@gmail.com>
2019-05-19 16:19:41 +0200

    Small improvements

--- wapuniverse-app/src/Editor.tsx
+++ wapuniverse-app/src/Editor.tsx
@@ -1,43 +1,23 @@
 import React from 'react';
 import * as PIXI from 'pixi.js';
-import yaml from 'js-yaml';
 import {autoResizingPixiApplication} from "./pixiUtils";
 
 import './Editor.css';
+import {fetchRezIndex, RezIndex} from "./rezIndex";
 
 class EditorMd {
 }
 
-interface Image {
-  offset: [number]; // [int, int]
-  path: string; // path to PID file inside REZ
-}
-
-interface ImageSet {
-  frames: { [frameIndex: number]: string } // -> pidFileName
-  sprites: { [pidFileName: string]: Image }
-}
-
-interface RezIndex {
-  imageSets: { [imageSetId: string]: ImageSet };
-}
-
-async function fetchRezIndex() {
-  const response = await fetch("rezIndex.yaml");
-  const text = await response.text();
-  const root = yaml.load(text) as RezIndex;
-  return root
-}
-
 async function loadResources(rezIndex: RezIndex): Promise<PIXI.loaders.ResourceDictionary> {
-  return new Promise(function (resolve, reject) {
+  return new Promise(resolve => {
     const loader = new PIXI.loaders.Loader();
     Object.entries(rezIndex.imageSets)
       .filter(([imageSetId,]) => imageSetId.startsWith("LEVEL1_"))
       .forEach(([, imageSet]) => {
           Object.values(imageSet.sprites).forEach((image) => {
-              const pngPath = "CLAW/" + image.path.replace(".PID", ".png");
-              loader.add(pngPath)
+              const pidPath = image.path;
+              const pngPath = "CLAW/" + pidPath.replace(".PID", ".png");
+              loader.add(pidPath, pngPath);
             }
           );
         }
@@ -62,7 +42,7 @@ class Editor extends React.Component {
   async init() {
     const rezIndex = await fetchRezIndex();
     const resources = await loadResources(rezIndex);
-    const officerTexture = resources["CLAW/LEVEL1/IMAGES/OFFICER/FRAME001.png"];
+    const officerTexture = resources["LEVEL1/IMAGES/OFFICER/FRAME001.PID"];
     this.application!.stage.addChild(new PIXI.Sprite(officerTexture.texture));
   }
 
