2f9e95d8c6c0719fb664716a3d7a94d986df584f
Jakub Trzebiatowski <cubuspl42@gmail.com>
2019-10-27 20:56:43 +0100

    Add area selection feedback effect

--- wapuniverse-app/src/Editor.ts
+++ wapuniverse-app/src/Editor.ts
@@ -2,20 +2,46 @@ import {fetchRezIndex, Image, RezIndex} from "./rezIndex";
 import {LevelResources} from "./LevelResources";
 import {Cell, CellSink} from "sodiumjs";
 import * as PIXI from 'pixi.js';
+import {Vec2} from "./Vec2";
 
 type Texture = PIXI.Texture;
-type Rectangle = PIXI.Rectangle;
 
-const Rectangle = PIXI.Rectangle;
+class Rectangle {
+  readonly xMin: number;
+  readonly yMin: number;
+  readonly width: number;
+  readonly height: number;
 
-class Vec2 {
-  readonly x: number;
-  readonly y: number;
-
-  constructor(x: number, y: number) {
-    this.x = x;
-    this.y = y;
+  get xMax(): number {
+    return this.xMin + this.width;
   }
+
+  get yMax(): number {
+    return this.yMin + this.height;
+  }
+
+  constructor(xMin: number, yMin: number, width: number, height: number) {
+    // if (width < 0) {
+    //   throw new Error('`width` must be >= 0');
+    // }
+    // if (height < 0) {
+    //   throw new Error('`height` must be >= 0');
+    // }
+    this.xMin = xMin;
+    this.yMin = yMin;
+    this.width = width;
+    this.height = height;
+  }
+
+  overlaps(b: Rectangle): boolean {
+    return this.xMin < b.xMax && b.xMin < this.xMax &&
+      this.yMin < b.yMax && b.yMin < this.yMax;
+  }
+
+  toString() {
+    return `(xMin: ${this.xMin}, yMin: ${this.yMin}, width: ${this.width}, height: ${this.height}, xMax: ${this.xMax}, yMax: ${this.yMax}`;
+  }
+
 }
 
 export class EdObject {
@@ -27,9 +53,12 @@ export class EdObject {
 
   readonly isHovered: CellSink<boolean>;
 
+  readonly isInSelectionArea: Cell<boolean>;
+
   constructor(
     rezIndex: RezIndex,
     levelResources: LevelResources,
+    areaSelection: Cell<AreaSelection | null>,
     initialPosition: Vec2,
     initialImageSet: string
   ) {
@@ -56,30 +85,71 @@ export class EdObject {
     const texture = rezImage.map(getTexture);
     const boundingBox = position.lift3(rezImage, texture, calculateBoundingBox);
 
+    const isInSelectionArea = Cell.switchC(areaSelection.map(a => {
+      return a !== null ?
+        a.objectsInArea.map(o => o.indexOf(this) !== -1) :
+        new Cell(false);
+    }));
+
     this.position = position;
     this.texture = texture;
     this.boundingBox = boundingBox;
     this.isHovered = new CellSink<boolean>(false);
+    this.isInSelectionArea = isInSelectionArea;
   }
-
 }
 
 export class App {
   editor = Editor.create();
 }
 
+export class AreaSelection {
+  readonly rectangle: Cell<Rectangle>;
+
+  readonly objectsInArea: Cell<ReadonlyArray<EdObject>>;
+
+  private readonly _onDone: () => void;
+
+  constructor(
+    origin: Vec2,
+    destination: Cell<Vec2>,
+    objects: ReadonlyArray<EdObject>, onDone: () => void
+  ) {
+    this._onDone = onDone;
+    const area = destination.map(d => {
+      return new Rectangle(
+        origin.x, origin.y, d.x - origin.x, d.y - origin.y);
+    });
+    this.rectangle = area;
+    this.objectsInArea = area.map((area) => {
+      return objects.filter((object) => {
+        const boundingBox = object.boundingBox.sample();
+        return area.overlaps(boundingBox);
+      }) as ReadonlyArray<EdObject>;
+    });
+  }
+
+  commit() {
+    this._onDone();
+  }
+}
+
 export class Editor {
   readonly objects: ReadonlyArray<EdObject>;
+
+  private _areaSelection = new CellSink<AreaSelection | null>(null);
+
+  readonly areaSelection = this._areaSelection as Cell<AreaSelection | null>;
 
   private constructor(rezIndex: RezIndex, levelResources: LevelResources) {
     this.objects = [
       new EdObject(
-        rezIndex, levelResources,
+        rezIndex, levelResources, this.areaSelection,
         new Vec2(64, 64),
         "LEVEL1_IMAGES_OFFICER"
       ),
       new EdObject(
-        rezIndex, levelResources,
+        rezIndex, levelResources, this.areaSelection,
         new Vec2(256, 256),
         "LEVEL1_IMAGES_SKULL"
       )
@@ -90,5 +160,12 @@ export class Editor {
     const rezIndex = await fetchRezIndex();
     const resources = await LevelResources.load(rezIndex, 1);
     return new Editor(rezIndex, resources);
+  }
+
+  selectByArea(origin: Vec2, destination: Cell<Vec2>) {
+    const areaSelection = new AreaSelection(origin, destination, this.objects, () => {
+      this._areaSelection.send(null);
+    });
+    this._areaSelection.send(areaSelection);
   }
 }
