bc4f84f0e072628c88673275735ed30ab87d2c77
Jakub Trzebiatowski <cubuspl42@gmail.com>
2019-10-27 20:56:43 +0100

    Add area selection feedback effect

--- wapuniverse-app/src/EditorUi.tsx
+++ wapuniverse-app/src/EditorUi.tsx
@@ -1,21 +1,25 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 
 import './Editor.css';
-import {Editor, EdObject} from "./Editor";
+import {AreaSelection, Editor, EdObject} from "./Editor";
 import {Container, Sprite, Stage} from "@inlet/react-pixi";
 import {GraphicsRectangle} from "./GraphicsRectangle";
-import {Cell} from "sodiumjs";
+import {Cell, StreamSink} from "sodiumjs";
+import {Vec2} from "./Vec2";
+
+type CellProvider<T> = () => Cell<T>;
 
 interface EditorUiProps {
   editor: Editor;
 }
 
-function useCell<T>(cell: Cell<T>) {
-  const [value, setValue] = useState(cell.sample());
+function useCell<T>(cell: Cell<T> | CellProvider<T>) {
+  const cell_ = useMemo(
+    cell instanceof Cell ? () => cell : cell,
+    undefined);
+  const [value, setValue] = useState(cell_.sample());
 
-  useEffect(() => {
-    return cell.listen(setValue);
-  }, [cell]);
+  useEffect(() => cell_.listen(setValue));
 
   return value;
 }
@@ -24,10 +28,12 @@ function EdObjectUi(object: EdObject) {
   const texture = useCell(object.texture);
   const boundingBox = useCell(object.boundingBox);
   const isHovered = useCell(object.isHovered);
+  const isInSelectionArea = useCell(object.isInSelectionArea);
+
   return <Container>
     <Sprite
-      x={boundingBox.x}
-      y={boundingBox.y}
+      x={boundingBox.xMin}
+      y={boundingBox.yMin}
       texture={texture}
       interactive={true}
       pointerover={() => {
@@ -37,15 +43,56 @@ function EdObjectUi(object: EdObject) {
         object.isHovered.send(false);
       }}
     />
-    <GraphicsRectangle x={boundingBox.x} y={boundingBox.y} width={boundingBox.width} height={boundingBox.height}
+    <GraphicsRectangle x={boundingBox.xMin} y={boundingBox.yMin}
+                       width={boundingBox.width} height={boundingBox.height}
                        strokeWidth={2}
-                       strokeColor={isHovered ? 0xFF0000 : 0x0000AA}/>
+                       strokeColor={isInSelectionArea ? 0xcd0000 :
+                         isHovered ? 0x0000cd : 0x87cefa
+                       }/>
   </Container>;
 }
 
+
 export function EditorUi({editor}: EditorUiProps) {
-  return <Stage width={1024} height={768}>
+  const areaSelection = useCell(editor.areaSelection);
+  const [areaSelectionDestination, setAreaSelectionDestination] =
+    useState<StreamSink<Vec2>>();
+
+  return <Stage
+    width={1024}
+    height={768}
+    onPointerDown={e => {
+      const origin = new Vec2(e.clientX, e.clientY);
+      const destinationS = new StreamSink<Vec2>();
+      setAreaSelectionDestination(destinationS);
+      const destination = new Cell(origin, destinationS);
+      editor.selectByArea(origin, destination);
+    }}
+    onPointerMove={e => {
+      if (areaSelectionDestination !== undefined) {
+        areaSelectionDestination.send(new Vec2(e.clientX, e.clientY));
+      }
+    }}
+    onPointerUp={e => {
+      if (areaSelection != null) {
+        areaSelection.commit();
+      }
+    }}
+  >
     {editor.objects.map(EdObjectUi)}
-  </Stage>
+    {areaSelection !== null && <AreaSelectionRectangle areaSelection={areaSelection}/>}
+  </Stage>;
 }
 
+interface AreaSelectionRectangleProps {
+  areaSelection: AreaSelection;
+}
+
+export function AreaSelectionRectangle(
+  {areaSelection}: AreaSelectionRectangleProps
+) {
+  const rectangle = useCell(areaSelection.rectangle);
+  return <GraphicsRectangle x={rectangle.xMin} y={rectangle.yMin}
+                            width={rectangle.width} height={rectangle.height}
+                            strokeWidth={1} strokeColor={0x00FFF0}/>
+}
