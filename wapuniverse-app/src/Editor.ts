9c6fe74423337646174aaecad8cd747d18fb45d6
Jakub Trzebiatowski <cubuspl42@gmail.com>
2019-10-28 22:34:55 +0100

    Implement object selection

--- wapuniverse-app/src/Editor.ts
+++ wapuniverse-app/src/Editor.ts
@@ -6,10 +6,24 @@ import {EdObject} from "./EdObject";
 import {AreaSelection} from "./AreaSelection";
 
 export class App {
-  editor = Editor.create();
+  editor = EditorInternal.create();
 }
 
-export class Editor {
+export interface Editor {
+  readonly selectedObjects: Cell<ReadonlyArray<EdObject>>;
+
+  readonly objects: ReadonlyArray<EdObject>;
+
+  readonly areaSelection: Cell<AreaSelection | null>;
+
+  selectByArea(origin: Vec2, destination: Cell<Vec2>): void;
+}
+
+export class EditorInternal implements Editor {
+  private _selectedObjects = new CellSink<ReadonlyArray<EdObject>>([]);
+
+  readonly selectedObjects = this._selectedObjects as Cell<ReadonlyArray<EdObject>>;
+
   readonly objects: ReadonlyArray<EdObject>;
 
   private _areaSelection = new CellSink<AreaSelection | null>(null);
@@ -19,11 +33,13 @@ export class Editor {
   private constructor(rezIndex: RezIndex, levelResources: LevelResources) {
     this.objects = [
       new EdObject(
+        this,
         rezIndex, levelResources, this.areaSelection,
         new Vec2(64, 64),
         "LEVEL1_IMAGES_OFFICER"
       ),
       new EdObject(
+        this,
         rezIndex, levelResources, this.areaSelection,
         new Vec2(256, 256),
         "LEVEL1_IMAGES_SKULL"
@@ -34,13 +50,22 @@ export class Editor {
   static async create(): Promise<Editor> {
     const rezIndex = await fetchRezIndex();
     const resources = await LevelResources.load(rezIndex, 1);
-    return new Editor(rezIndex, resources);
+    return new EditorInternal(rezIndex, resources);
   }
 
-  selectByArea(origin: Vec2, destination: Cell<Vec2>) {
-    const areaSelection = new AreaSelection(origin, destination, this.objects, () => {
-      this._areaSelection.send(null);
-    });
+  selectByArea(origin: Vec2, destination: Cell<Vec2>): void {
+    const areaSelection = new AreaSelection(
+      this,
+      origin,
+      destination,
+      this.objects,
+      () => {
+        this._areaSelection.send(null);
+      });
     this._areaSelection.send(areaSelection);
+  }
+
+  selectObjects(objects: ReadonlyArray<EdObject>) {
+    this._selectedObjects.send(objects);
   }
 }
