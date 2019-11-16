9bda8ef6dbbe4e22d3ce6fce68aa4e8c04782d66
Jakub Trzebiatowski <cubuspl42@gmail.com>
2019-11-16 12:28:46 +0100

    Add wwd.test.ts

---
+++ wapuniverse-app/src/wwd.test.ts
@@ -0,0 +1,15 @@
+import {readWwd} from "./wwd";
+import * as fs from "fs";
+import * as path from "path";
+
+const projectDir = path.join(__dirname, "..");
+
+function readWorldBuffer(levelIndex: number): ArrayBuffer {
+  const worldPath = path.join(projectDir, "data", "CLAW.REZ", `LEVEL${levelIndex}`, "WORLDS", "WORLD.WWD");
+  return fs.readFileSync(worldPath).buffer;
+}
+
+it('reads LEVEL1.WWD', () => {
+  const worldBuffer = readWorldBuffer(1);
+  const wwd = readWwd(worldBuffer);
+});
