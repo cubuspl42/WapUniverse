3a8e139098885567dfa80ddcfd4c6207e623bbb9
Jakub Trzebiatowski <cubuspl42@gmail.com>
2019-12-02 21:45:33 +0100

    Move EdObjectUi to a separate file

--- wapuniverse-app/src/EditorUi.tsx
+++ wapuniverse-app/src/EditorUi.tsx
@@ -3,15 +3,13 @@ import React, {useEffect, useMemo, useState} from 'react';
 import './Editor.css';
 import {Editor} from "./Editor";
 import {GraphicsRectangle} from "./GraphicsRectangle";
-import {EdObject} from "./EdObject";
 import {AreaSelection} from "./AreaSelection";
 import * as PIXI from 'pixi.js';
 import * as pu from "./pixiUtils";
 import {Vec2} from "./Vec2";
 import {Cell, CellSink} from "./Cell";
 import {StreamSink} from "sodiumjs";
-
-const Texture = PIXI.Texture;
+import {EdObjectUi} from "./EdObjectUi";
 
 type CellProvider<T> = () => Cell<T>;
 
@@ -36,84 +34,6 @@ export function useCell<T>(cell: Cell<T> | CellProvider<T>): T {
 
 export function range(end: number): Array<number> {
   return [...Array(end).keys()];
-}
-
-function getRandomInt(max: number) {
-  let min = 0;
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-interface EdObjectUiProps {
-  object: EdObject;
-}
-
-function EdObjectUi({object}: EdObjectUiProps): PIXI.DisplayObject {
-  const texture = object.texture;
-  const boundingBox = object.boundingBox;
-  const isHovered = object.isHovered;
-  const isInSelectionArea = object.isInSelectionArea;
-  const isSelected = object.isSelected;
-
-  const x = boundingBox.map((b) => b.xMin);
-  const y = boundingBox.map((b) => b.yMin);
-  const width = boundingBox.map((b) => b.width);
-  const height = boundingBox.map((b) => b.height);
-
-  const container = new PIXI.Container();
-
-  const sprite = pu.sprite({
-    x: x,
-    y: y,
-    texture: texture,
-    alpha: isHovered.map((h): number => h ? 1 : 0.5),
-  });
-
-  sprite.interactive = true;
-
-  sprite.on("pointerdown", () => {
-    const newI = object.i.sample() + 1;
-    console.log(`newI: ${newI}`);
-    object.i.send(newI);
-  });
-
-  sprite.on("pointerover", () => {
-    object.isHovered.send(true);
-  });
-
-  sprite.on("pointerout", () => {
-    object.isHovered.send(false);
-  });
-
-  container.addChild(sprite);
-
-  const frameRectangle = pu.graphicsRectangle({
-    x: x,
-    y: y,
-    width: width,
-    height: height,
-    strokeWidth: new CellSink(2),
-    strokeColor: isInSelectionArea.lift(isSelected,
-      (isInSelectionAreaV: boolean, isSelectedV: boolean): number =>
-        isInSelectionAreaV ? 0xcd0000 : isSelectedV ? 0xe3fc03 : 0x87cefa
-    ),
-  });
-
-  container.addChild(frameRectangle);
-
-  {/*{isHovered && <GraphicsRectangle key={`g2${object.id}`} x={boundingBox.xMin - 2} y={boundingBox.yMin - 2}*/
-  }
-  {/*                                 width={boundingBox.width + 4} height={boundingBox.height + 4}*/
-  }
-  {/*                                 strokeWidth={2}*/
-  }
-  {/*                                 strokeColor={0x0000cd}/>*/
-  }
-  {/*}*/
-  }
-
-  return container;
 }
 
 export class EditorUi extends React.Component<EditorUiProps> {
