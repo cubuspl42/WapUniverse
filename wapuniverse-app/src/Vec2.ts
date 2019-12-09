2c3d8f2afc004b5a9fe491f6793e391dbac8c1fc
Jakub Trzebiatowski <cubuspl42@gmail.com>
2019-12-09 20:56:43 +0100

    Implement scrolling and zooming

--- wapuniverse-app/src/Vec2.ts
+++ wapuniverse-app/src/Vec2.ts
@@ -1,3 +1,5 @@
+import * as PIXI from 'pixi.js';
+
 export class Vec2 {
   readonly x: number;
   readonly y: number;
@@ -7,7 +9,39 @@ export class Vec2 {
     this.y = y;
   }
 
+  add(other: Vec2): Vec2 {
+    return new Vec2(
+      this.x + other.x,
+      this.y + other.y,
+    );
+  }
+
+  sub(other: Vec2): Vec2 {
+    return new Vec2(
+      this.x - other.x,
+      this.y - other.y,
+    );
+  }
+
+  div(a: number) {
+    return new Vec2(
+      this.x / a,
+      this.y / a,
+    );
+  }
+
+  neg() {
+    return new Vec2(
+      -this.x,
+      -this.y,
+    );
+  }
+
   toString(): string {
     return `(${this.x}, ${this.y})`;
+  }
+
+  toPixiPoint(): PIXI.Point {
+    return new PIXI.Point(this.x, this.y);
   }
 }
---
+++ wapuniverse-app/src/cellUtils.ts
@@ -0,0 +1,12 @@
+import {Vec2} from "./Vec2";
+import {Cell, CellSink} from "./Cell";
+
+export function elementSize(element: HTMLElement): Cell<Vec2> {
+  const cell = new CellSink(new Vec2(element.offsetWidth, element.offsetHeight));
+
+  new ResizeObserver(() => {
+    cell.send(new Vec2(element.offsetWidth, element.offsetHeight))
+  }).observe(element);
+
+  return cell;
+}
